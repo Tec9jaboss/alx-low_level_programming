cf8d9c548666043e0a4898bb311a62c664885938
Tec9jaboss <tec9ja.com@gmail.com>
2023-02-16 13:51:21 -0800

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - print a text using printf
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
