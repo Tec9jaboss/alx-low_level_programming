a64d97bca1913930b4c4fb43560b841ceec4b45a
Tec9jaboss <tec9ja.com@gmail.com>
2023-03-10 14:27:45 -0800

    new

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-void _puts(char *str)
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void _puts(char *s)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+    puts(s);
 }
