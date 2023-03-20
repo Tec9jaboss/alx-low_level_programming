289118f24c1f5e73c167e5d7306562522f8c4d33
Tec9jaboss <tec9ja.com@gmail.com>
2023-03-20 16:42:48 -0700

    dof

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
 * struct dog - a dog's basic info
 * @name: First member
@@ -10,10 +11,11 @@
 */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
 /**
 * dog_t - typedef for struct dog
 */
